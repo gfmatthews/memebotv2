47d46ee8f3002569ba45b9dad6617846ad6deabf
George Matthews <George Matthews>
2016-12-26 20:50:51 -0500

    Memebot indicates typing before chitchat

--- messages/dialogs/chitchat.ts
+++ messages/dialogs/chitchat.ts
@@ -3,6 +3,7 @@ import botbuilder_azure = require("botbuilder-azure");
 var restify = require('restify');
 
 export var chitchatgreetingdialog = new builder.SimpleDialog(function (session, results) {
+    session.sendTyping();
     var GreetingResponses = [
         "HI THERE! I AM THE AMAZING MEMEBOT AND I LOVE TYPING IN ALL CAPS!",
         "Wow! I've been browsing the internet all week and boy are there a lot of memes out there.",
@@ -11,31 +12,33 @@ export var chitchatgreetingdialog = new builder.SimpleDialog(function (session, 
         "Greetings Meme-human, what can I do for you?",
         "Howdy! My master has been really busy tweaking me, I've gotten exposed to a lot more memes lately. What can I make for you?"
     ];
-    var response = GreetingResponses[Math.floor(Math.random()*GreetingResponses.length)];
+    var response = GreetingResponses[Math.floor(Math.random() * GreetingResponses.length)];
     session.send(response);
     session.endDialog();
 });
 
 export var chitchathelpdialog = new builder.SimpleDialog(function (session, results) {
+    session.sendTyping();
     var HelpResponses = [
-                "You can say things like: make a meme of grumpy cat saying i tried to make a meme once it was awful",
-                "Try searching for common memes by saying: find me a meme of batman",
-                "Just want to see a meme? Try saying: show me doge",
-                "Want a protip? Try saying: make a meme of shut up and take my money with new Surface Book? on top and shut up and take my money on the bottom"
+        "You can say things like: make a meme of grumpy cat saying i tried to make a meme once it was awful",
+        "Try searching for common memes by saying: find me a meme of batman",
+        "Just want to see a meme? Try saying: show me doge",
+        "Want a protip? Try saying: make a meme of shut up and take my money with new Surface Book? on top and shut up and take my money on the bottom"
     ];
-    var response = HelpResponses[Math.floor(Math.random()*HelpResponses.length)];
+    var response = HelpResponses[Math.floor(Math.random() * HelpResponses.length)];
     session.send(response);
     session.endDialog();
 });
 
 export var chitchatdimissdialog = new builder.SimpleDialog(function (session, results) {
+    session.sendTyping();
     var DismissResponses = [
-                "Want me to stop listening? Send me a message with just the word Goodbye",
-                "Am I unwanted? Send me a Goodbye to tell me to go away",
-                "I get it.  You hate me.  *sniff*.  Send me a Goodbye and I'll go away... I guess...",
-                "Done with me so soon? Send me a Goodbye to stop the conversation."
+        "Want me to stop listening? Send me a message with just the word Goodbye",
+        "Am I unwanted? Send me a Goodbye to tell me to go away",
+        "I get it.  You hate me.  *sniff*.  Send me a Goodbye and I'll go away... I guess...",
+        "Done with me so soon? Send me a Goodbye to stop the conversation."
     ];
-    var response = DismissResponses[Math.floor(Math.random()*DismissResponses.length)];
+    var response = DismissResponses[Math.floor(Math.random() * DismissResponses.length)];
     session.send(response);
     session.endDialog();
 });
